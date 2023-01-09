5a753ea7672e104449ed3c67e55435a622fb814f
Edward Wei <nyw6dh@virginia.edu>
2023-01-08 18:58:38 -0800

    Initial Commit

---
+++ pirategram/pirategramApp.swift
@@ -0,0 +1,17 @@
+//
+//  pirategramApp.swift
+//  pirategram
+//
+//  Created by Eddie Wei on 1/8/23.
+//
+
+import SwiftUI
+
+@main
+struct pirategramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
